d9811c066d337b74d20f8351dce714156f745541
rochman25 <zaenur.rochman98@gmail.com>
2021-10-16 11:49:38 +0700

    update main activity
    - add home layout

--- app/src/main/java/com/presensi/panda/MainActivity.kt
+++ app/src/main/java/com/presensi/panda/MainActivity.kt
@@ -2,10 +2,38 @@ package com.presensi.panda
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.presensi.panda.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val listAttendance = ArrayList<Attendance>()
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.hide()
+
+        binding.rvAttendances.setHasFixedSize(true)
+        listAttendance.addAll(listAttendances)
+        showRecyclerList()
+    }
+
+    private val listAttendances: ArrayList<Attendance>
+        get() {
+            val listData = ArrayList<Attendance>()
+            for(i in 1..10){
+                var attendance = Attendance("Sabtu","13 Oktober 2021","08:00","12:00")
+                listData.add(attendance)
+            }
+            return listData
+        }
+
+    private fun showRecyclerList() {
+        binding.rvAttendances.layoutManager = LinearLayoutManager(this)
+        val listAttendanceAdapter = ListAttendanceAdapter(listAttendance)
+        binding.rvAttendances.adapter = listAttendanceAdapter
     }
 }
