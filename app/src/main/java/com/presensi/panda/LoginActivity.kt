7ee699805cdd180bfd6ddef15554a090eb10cc5e
rochman25 <zaenur.rochman98@gmail.com>
2021-10-21 16:06:25 +0700

    add form validation login

--- app/src/main/java/com/presensi/panda/LoginActivity.kt
+++ app/src/main/java/com/presensi/panda/LoginActivity.kt
@@ -3,6 +3,7 @@ package com.presensi.panda
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.widget.doOnTextChanged
 import com.presensi.panda.databinding.ActivityLoginBinding
 import com.presensi.panda.databinding.ActivityMainBinding
 
@@ -14,10 +15,35 @@ class LoginActivity : AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
         supportActionBar?.hide()
+        
+        binding.txtUsername.doOnTextChanged { text, start, before, count -> binding.outlineUsername.isErrorEnabled = false }
+        binding.txtPassword.doOnTextChanged { text, start, before, count -> binding.outlinePassword.isErrorEnabled = false }
 
         binding.btnLogin.setOnClickListener{
-            val moveMainActivity = Intent(this@LoginActivity, MainActivity::class.java)
-            startActivity(moveMainActivity)
+            if(validateData()){
+                val moveMainActivity = Intent(this@LoginActivity, MainActivity::class.java)
+                startActivity(moveMainActivity)
+            }
         }
     }
+
+    private fun validateData(): Boolean {
+        var isValid = true;
+        val username = binding.txtUsername.text.toString()
+        val password = binding.txtPassword.text.toString()
+
+        if(username.isEmpty()){
+            isValid = false
+            binding.outlineUsername.error = "Username tidak boleh kosong."
+        }
+
+        if(password.isEmpty()){
+            isValid = false
+            binding.outlinePassword.error = "Password tidak boleh kosong."
+        }
+
+        return isValid
+    }
+
+
 }
