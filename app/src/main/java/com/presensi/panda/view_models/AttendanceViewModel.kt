b361106e5ed2d2cae9de0e3f75a84818d89e7ae1
rochman25 <zaenur.rochman98@gmail.com>
2021-11-01 10:51:56 +0700

    fix convertion date to day

--- app/src/main/java/com/presensi/panda/view_models/AttendanceViewModel.kt
+++ app/src/main/java/com/presensi/panda/view_models/AttendanceViewModel.kt
@@ -1,7 +1,9 @@
 package com.presensi.panda.view_models
 
 import android.content.Context
+import android.os.Build
 import android.util.Log
+import androidx.annotation.RequiresApi
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -13,6 +15,8 @@ import retrofit2.Callback
 import retrofit2.Response
 import java.text.SimpleDateFormat
 import java.time.LocalDate
+import java.time.LocalDateTime
+import java.util.*
 import kotlin.collections.ArrayList
 
 class AttendanceViewModel : ViewModel() {
@@ -29,10 +33,8 @@ class AttendanceViewModel : ViewModel() {
             ) {
                 val responseBody = response.body()
                 val listAttendances = ArrayList<Attendance>()
-                val sdf = SimpleDateFormat("dd")
                 for(item in responseBody?.data!!){
-                    var currentDate = sdf.parse(item?.localDate.toString())
-                    var attendance = Attendance(day[currentDate.day],item?.localDate.toString(),item?.checkIn,item?.checkOut)
+                    var attendance = Attendance(day[convertDate(item?.localDate.toString())].toString(),item?.localDate.toString(),item?.checkIn,item?.checkOut)
                     listAttendances.add(attendance)
                 }
                 _listLog.value = listAttendances
@@ -46,9 +48,42 @@ class AttendanceViewModel : ViewModel() {
         })
     }
 
+    private fun convertDate(localDate: String) : Int{
+        val localDate = localDate.split("-")
+        val day = localDate[0].toInt()
+        val currentMonth = month[localDate[1]]
+        val year = localDate[2].toInt()
+
+        val calendar = Calendar.getInstance()
+
+        calendar.set(year, currentMonth!!, day)
+
+        return calendar.get(Calendar.DAY_OF_WEEK)
+    }
+
     companion object {
         private const val TAG = "AttendanceViewModel"
-        private val day = arrayOf("Senin","Selasa","Rabu","Kamis","Jum'at","Sabtu","Minggu")
+        private val day = mapOf(Calendar.MONDAY to "Senin",
+            Calendar.TUESDAY to "Selasa",
+            Calendar.WEDNESDAY to "Rabu",
+            Calendar.THURSDAY to "Kamis",
+            Calendar.FRIDAY to "Jum'at",
+            Calendar.SATURDAY to "Sabtu",
+            Calendar.SUNDAY to "Minggu")
+
+        val month = mapOf("Jan" to 0,
+            "Feb" to 1,
+            "Mar" to 2,
+            "Apr" to 3,
+            "May" to 4,
+            "Jun" to 5,
+            "Jul" to 6,
+            "Aug" to 7,
+            "Sep" to 8,
+            "Oct" to 9,
+            "Nov" to 10,
+            "Dec" to 11
+            )
     }
 
 }
